eced4f4349e444bb9c7469b3e1ba5bb4bc1e4dc4
Ryan Stalbow <ryan.stalbow@uktv.co.uk>
2017-10-17 14:31:34 +0100

    fixes for django 1.11

--- management_logging/urls.py
+++ management_logging/urls.py
@@ -1,10 +1,9 @@
-from django.conf.urls import url, patterns
+from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 
 from .views import ManagementLoggingReport
 
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^$', login_required(ManagementLoggingReport.as_view()), name='management_logging_report'),
-)
+]
