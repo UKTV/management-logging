e0efd152d10bb1f4d8abb90d92b804b91b0012eb
PhysTom88 <tom.ravenscroft@uktv.co.uk>
2019-05-13 17:13:54 +0100

    added a renderer to the widget

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='management_logging',
-    version='1.2.3',
+    version='1.2.4',
     author='Ryan Stalbow',
     author_email='ryan.stalbow@uktv.co.uk',
     packages=['management_logging'],
